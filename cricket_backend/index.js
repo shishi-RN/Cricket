514a9eebb506ad1d01f4a03bd47ecb2b6067f574
shijoavb <shishijo@avaibe.com>
2025-02-12 11:14:45 +0530

    changes

---
+++ cricket_backend/index.js
@@ -0,0 +1,219 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const Fuse = require("fuse.js");
+const app = express();
+app.use(cors());
+const images = [
+  {
+    name: "IND",
+    image: "https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg",
+  },
+  {
+    name: "ENG",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a5/Flag_of_the_United_Kingdom_(1-2).svg",
+  },
+  {
+    name: "Rohit Sharma (c)",
+    image:
+      "https://raw.githubusercontent.com/hdpngworld/HPW/main/uploads/6515d0d42f40b-c.png",
+  },
+  {
+    name: "Shubman Gill",
+    image:
+      "https://www.pngguru.in/storage/uploads/images/Shubman%20Gill%20Indian%20cricket%20player%20free%20PNG%20photo_1696590812_1798805955.webp",
+  },
+  {
+    name: "Virat Kohli",
+    image:
+      "https://w7.pngwing.com/pngs/400/994/png-transparent-virat-kohli-thumbnail.png",
+  },
+  {
+    name: "Shreyas Iyer",
+    image:
+      "https://static-files.cricket-australia.pulselive.com/headshots/440/10922-camedia.png",
+  },
+  {
+    name: "KL Rahul",
+    image:
+      "https://i.pinimg.com/474x/48/37/af/4837af17a9f1bf260a2a1065332d9cd1.jpg",
+  },
+  {
+    name: "Hardik Pandya",
+    image:
+      "https://www.pngguru.in/storage/uploads/images/hardik-pandya-indian-cricketer-free-hd-transparent-png-photo_1719995107_1895634568.webp",
+  },
+  {
+    name: "Ravindra Jadeja",
+    image:
+      "https://cricclubs.com/documentsRep/profilePics/a7c304ce-5c09-4ad9-b495-e9f756ed756d.png",
+  },
+  {
+    name: "Axar Patel",
+    image:
+      "https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_1200,q_60/lsci/db/PICTURES/CMS/331100/331164.png",
+  },
+  {
+    name: "Kuldeep Yadav",
+    image:
+      "https://static-files.cricket-australia.pulselive.com/headshots/288/10953-camedia.png",
+  },
+  {
+    name: "Mohammed Shami",
+    image:
+      "https://www.pngguru.in/storage/uploads/images/Mohammed%20Shami%20Indian%20cricket%20player%20HD%20PNG%20Image_1700145250_290197030.webp",
+  },
+  {
+    name: "Harshit Rana",
+    image:
+      "https://static-files.cricket-australia.pulselive.com/headshots/288/10208-camedia.png",
+  },
+  {
+    name: "Yashasvi Jaiswal",
+    image:
+      "https://static-files.cricket-australia.pulselive.com/headshots/440/2993-camedia.png",
+  },
+  {
+    name: "Rishabh Pant",
+    image:
+      "https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_1200,q_60/lsci/db/PICTURES/CMS/323000/323036.png",
+  },
+  {
+    name: "Varun Chakravarthy",
+    image:
+      "https://img1.hscicdn.com/image/upload/f_auto/lsci/db/PICTURES/CMS/331100/331167.png",
+  },
+
+  {
+    name: "Jos Buttler (c)",
+    image:
+      "https://i.pinimg.com/564x/11/98/64/119864a2399eff84536f86bcf9fb39d8.jpg",
+  },
+  {
+    name: "Philip Salt",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzH_PkECzuLRSkHjO7kMp2CmEM7c0fOpgSsw&s",
+  },
+  {
+    name: "Ben Duckett",
+    image:
+      "https://static-files.cricket-australia.pulselive.com/headshots/288/67-test.png",
+  },
+  {
+    name: "Joe Root",
+    image: "https://www.espncricinfo.com/inline/content/image/1183220.html",
+  },
+  {
+    name: "Harry Brook",
+    image: "https://www.espncricinfo.com/inline/content/image/1183221.html",
+  },
+  {
+    name: "Liam Livingstone",
+    image: "https://www.espncricinfo.com/inline/content/image/1183222.html",
+  },
+  {
+    name: "Jamie Smith",
+    image: "https://www.espncricinfo.com/inline/content/image/1183223.html",
+  },
+  {
+    name: "Jamie Overton",
+    image: "https://www.espncricinfo.com/inline/content/image/1183224.html",
+  },
+  {
+    name: "Jacob Bethell",
+    image: "https://www.espncricinfo.com/inline/content/image/1183225.html",
+  },
+  {
+    name: "Gus Atkinson",
+    image: "https://www.espncricinfo.com/inline/content/image/1183226.html",
+  },
+  {
+    name: "Jofra Archer",
+    image: "https://www.espncricinfo.com/inline/content/image/1183227.html",
+  },
+  {
+    name: "Saqib Mahmood",
+    image: "https://www.espncricinfo.com/inline/content/image/1183228.html",
+  },
+  {
+    name: "Adil Rashid",
+    image: "https://www.espncricinfo.com/inline/content/image/1183229.html",
+  },
+  {
+    name: "Brydon Carse",
+    image: "https://www.espncricinfo.com/inline/content/image/1183230.html",
+  },
+  {
+    name: "Mark Wood",
+    image: "https://www.espncricinfo.com/inline/content/image/1183231.html",
+  },
+  {
+    name: "Tom Banton",
+    image: "https://www.espncricinfo.com/inline/content/image/1183232.html",
+  },
+];
+
+function getPlayerImage(playerName) {
+  const options = {
+    keys: ["name"],
+    threshold: 0.3, // Adjust this value based on desired matching sensitivity
+  };
+  const fuse = new Fuse(images, options);
+  const result = fuse.search(playerName);
+  if (result.length > 0) {
+    return result[0].item.image;
+  }
+  return null; // Return null if no close match is found
+}
+
+app.get("/match-data", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://www.cricbuzz.com/api/cricket-match/commentary/100321"
+    );
+    const { matchHeader, commentaryList, miniscore } = response.data;
+
+    // Retrieve player images using fuzzy matching
+    const batsmanNonStrikerImage = getPlayerImage(
+      miniscore.batsmanNonStriker.batName
+    );
+    const batsmanStrikerImage = getPlayerImage(
+      miniscore.batsmanStriker.batName
+    );
+    const bowlerImage = getPlayerImage(miniscore.bowlerStriker.bowlName);
+    const match = miniscore.matchScoreDetails.inningsScoreList.map((inning) => ({
+      ...inning,
+      teamImage: getPlayerImage(inning.batTeamName),
+    }));
+    console.log(match)
+    const api = {
+      status: matchHeader.status,
+      toss: matchHeader.tossResults,
+      commentary: commentaryList[0].commText,
+      partnership: miniscore.partnerShip,
+      batsmanNonStriker: {
+        ...miniscore.batsmanNonStriker,
+        image: batsmanNonStrikerImage,
+      },
+      batsmanStriker: {
+        ...miniscore.batsmanStriker,
+        image: batsmanStrikerImage,
+      },
+      bowler: {
+        ...miniscore.bowlerStriker,
+        image: bowlerImage,
+      },
+      event: miniscore.event,
+      lastWicket: miniscore.lastWicket,
+     match,
+     recentOvsStats:miniscore.recentOvsStats
+    };
+
+    res.send(api);
+  } catch (error) {
+    res.status(500).send("Error fetching data");
+  }
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
--- cricket_frontend/App.web.tsx
+++ cricket_frontend/App.web.tsx
@@ -1,85 +1,208 @@
-import React from 'react';
-import { 
-  Animated, 
-  StyleSheet, 
-  View, 
-  Text, 
+import React, {useEffect, useState, useRef} from 'react';
+import {
+  Animated,
+  StyleSheet,
+  View,
+  Text,
   Dimensions,
-  PixelRatio 
+  PixelRatio,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import axios from 'axios';
 
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
 // Responsive scaling functions
 const scaleFont = size => {
   const scaleFactor = PixelRatio.getFontScale();
-  return PixelRatio.roundToNearestPixel(size * Math.min(width / 375, height / 667) * scaleFactor);
+  return PixelRatio.roundToNearestPixel(
+    size * Math.min(width / 375, height / 667) * scaleFactor,
+  );
 };
 
 const scaleSize = size => PixelRatio.roundToNearestPixel(size * (width / 375));
-const scaleHorizontal = size => PixelRatio.roundToNearestPixel(size * (width / 375));
-const scaleVertical = size => PixelRatio.roundToNearestPixel(size * (height / 667));
+const scaleHorizontal = size =>
+  PixelRatio.roundToNearestPixel(size * (width / 375));
+const scaleVertical = size =>
+  PixelRatio.roundToNearestPixel(size * (height / 667));
 
-// Flag aspect ratio (3:2)
-const FLAG_ASPECT_RATIO = 3/2;
+const FLAG_ASPECT_RATIO = 3 / 2;
 
 const App = () => {
+  const [matchData, setMatchData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Animation values
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const rotationAnim = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(rotationAnim, {
+        toValue: 1,
+        duration: 2000, // Rotation duration in milliseconds
+        useNativeDriver: true,
+      }),
+    ).start();
+  }, [rotationAnim]);
+
+  const apiData = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:3000/match-data');
+      console.log(response.data, 'heereee');
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await apiData();
+        setMatchData(data);
+        setLoading(false);
+
+        // Start the fade-in animation after data is loaded
+        Animated.timing(fadeAnim, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: true,
+        }).start();
+      } catch (error) {
+        setError('Error fetching data');
+        setLoading(false);
+      }
+    };
+
+    // Fetch data immediately on component mount
+    fetchData();
+
+    // Set up interval to fetch data every 5 seconds
+    const intervalId = setInterval(fetchData, 5000);
+
+    // Clean up interval on component unmount
+    return () => clearInterval(intervalId);
+  }, [fadeAnim]);
+
+  const indInnings = matchData?.match?.find(m => m.batTeamName === 'IND');
+  const engInnings = matchData?.match?.find(m => m.batTeamName === 'ENG');
+  const partnershipText = `${matchData?.partnership.runs}(${matchData?.partnership.balls})`;
+  const tossText = `${matchData?.toss.tossWinnerName} won toss & chose ${matchData?.toss.decision}`;
+  const striker = matchData?.batsmanStriker;
+  const nonStriker=matchData?.batsmanNonStriker;
+  const bowler =matchData?.bowler
+  const strikerText = `${striker?.batName}: ${striker?.batRuns}(${striker?.batBalls})`;
+  const nonStrikeText=`${nonStriker?.batName}: ${nonStriker?.batRuns}(${nonStriker?.batBalls})`
+  const strikeRateText = `Strike Rate: ${striker?.batStrikeRate.toFixed(2)}`;
+  const foursSixesText = `4s/6s: ${striker?.batFours}/${striker?.batSixes}`;
+  const nonstrikeRateText = `Strike Rate: ${nonStriker?.batStrikeRate.toFixed(2)}`;
+  const nonfoursSixesText = `4s/6s: ${nonStriker?.batFours}/${nonStriker?.batSixes}`;
+const bowlerRate= `Overs : ${bowler?.bowlOvs} Runs : ${bowler?.bowlRuns}  Wickets : ${bowler?.bowlWkts} Economy : ${bowler?.bowlEcon}`
   return (
     <View style={styles.container}>
       {/* Top Section - Match Status */}
       <LinearGradient
         colors={['#000000', '#434343']}
-        style={[styles.box, { flex: 0.3 * (height / 667) }]}>
+        style={[styles.box, {flex: 0.3 * (height / 667)}]}>
         <View style={styles.flagContainer}>
           <Animated.Image
-            source={{ uri: 'https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg' }}
+            source={{uri: indInnings?.teamImage}}
             style={[
-              styles.flag, 
-              { 
+              styles.flag,
+              {
                 width: width * 0.15,
-                height: (width * 0.20) / FLAG_ASPECT_RATIO,
+                height: (width * 0.2) / FLAG_ASPECT_RATIO,
                 borderWidth: scaleSize(2),
-                borderRadius: scaleSize(4)
-              }
+                borderRadius: scaleSize(4),
+              },
             ]}
             resizeMode="cover"
           />
-          
-          <View style={styles.scoreContainer}>
-            <Text style={[styles.teamName, { fontSize: scaleFont(32) }]}>IND</Text>
-            <Text style={[styles.score, { fontSize: scaleFont(42) }]}>232-3</Text>
-            <Text style={[styles.over, { fontSize: scaleFont(32) }]}>Over: 32.1</Text>
-          </View>
-
-          <View style={[
-            styles.progressContainer,
-            { 
-              width: scaleHorizontal(40),
-              borderRadius: scaleSize(30),
-              borderWidth: scaleSize(3),
-              marginTop: scaleVertical(20)
-            }
-          ]}>
-            <View style={styles.progressBar} />
-          </View>
 
           <View style={styles.scoreContainer}>
-            <Text style={[styles.teamName, { fontSize: scaleFont(32) }]}>ENG</Text>
-            <Text style={[styles.score, { fontSize: scaleFont(42) }]}>232-3</Text>
-            <Text style={[styles.over, { fontSize: scaleFont(32) }]}>Over: 32.1</Text>
+            <Animated.Text
+              style={[
+                styles.teamName,
+                {fontSize: scaleFont(32), opacity: fadeAnim},
+              ]}>
+              {indInnings?.batTeamName}
+            </Animated.Text>
+            <Animated.Text
+              style={[
+                styles.score,
+                {fontSize: scaleFont(42), opacity: fadeAnim},
+              ]}>
+              {indInnings?.score}/{indInnings?.wickets}
+            </Animated.Text>
+            <Animated.Text
+              style={[
+                styles.over,
+                {fontSize: scaleFont(32), opacity: fadeAnim},
+              ]}>
+              Over: {indInnings?.overs}
+            </Animated.Text>
+          </View>
+
+          <View
+            style={[
+              styles.progressContainer,
+              {
+                width: scaleHorizontal(60),
+                borderRadius: scaleSize(30),
+                borderWidth: scaleSize(1),
+                marginTop: scaleVertical(20),
+              },
+            ]}>
+            <Animated.Text
+              style={[
+                styles.teamName,
+                {
+                  fontSize: scaleFont(32),
+                  justifyContent: 'center',
+                  alignSelf: 'center',
+                  opacity: rotationAnim,
+                },
+              ]}>
+              {matchData?.event}
+            </Animated.Text>
+            {/* <View style={styles.progressBar} /> */}
+          </View>
+
+          <View style={styles.scoreContainer}>
+            <Animated.Text
+              style={[
+                styles.teamName,
+                {fontSize: scaleFont(32), opacity: fadeAnim},
+              ]}>
+              {engInnings?.batTeamName}
+            </Animated.Text>
+            <Animated.Text
+              style={[
+                styles.score,
+                {fontSize: scaleFont(42), opacity: fadeAnim},
+              ]}>
+              {engInnings?.score}/{engInnings?.wickets}
+            </Animated.Text>
+            <Animated.Text
+              style={[
+                styles.over,
+                {fontSize: scaleFont(32), opacity: fadeAnim},
+              ]}>
+              Over: {engInnings?.overs}
+            </Animated.Text>
           </View>
 
           <Animated.Image
-            source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Flag_of_the_United_Kingdom_(1-2).svg' }}
+            source={{uri: engInnings?.teamImage}}
             style={[
-              styles.flag, 
-              { 
+              styles.flag,
+              {
                 width: width * 0.15,
-                height: (width * 0.20) / FLAG_ASPECT_RATIO,
+                height: (width * 0.2) / FLAG_ASPECT_RATIO,
                 borderWidth: scaleSize(2),
-                borderRadius: scaleSize(4)
-              }
+                borderRadius: scaleSize(4),
+              },
             ]}
             resizeMode="cover"
           />
@@ -89,73 +212,104 @@ const App = () => {
       {/* Stats Bar */}
       <LinearGradient
         colors={['#0f2027', '#203a43']}
-        style={[styles.box, { flex: 0.1 * (height / 667) }]}>
-        <Text style={[styles.statsText, { fontSize: scaleFont(28) }]}>
-          Partnership: 72(52)  |  Last 5 Ov: 54/0
+        style={[styles.box, {flex: 0.1 * (height / 667)}]}>
+        <Text
+          style={[
+            styles.statsText,
+            {fontSize: scaleFont(20), marginTop: scaleFont(19)},
+          ]}>
+          Partnership: {partnershipText} | Last 5 Ov:{' '}
+          {matchData?.recentOvsStats}
         </Text>
       </LinearGradient>
 
       {/* Main Content Area */}
       <LinearGradient
         colors={['#3e0000', '#7a0000']}
-        style={[styles.box, { flex: 0.4 * (height / 667) }]}>
+        style={[styles.box, {flex: 0.4 * (height / 667)}]}>
         <View style={styles.backgroundContainer}>
           <Animated.Image
-            source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/6/6e/Wankhede_ICC_WCF.jpg' }}
+            source={{
+              uri: 'https://upload.wikimedia.org/wikipedia/commons/6/6e/Wankhede_ICC_WCF.jpg',
+            }}
             style={[
               styles.backgroundImage,
-              { 
-                width: width - scaleHorizontal(16),
+              {
+                width: width,
                 height: height * 0.4,
-                borderRadius: scaleSize(8)
-              }
+                borderRadius: scaleSize(8),
+              },
             ]}
             resizeMode="cover"
           />
         </View>
         <View style={styles.overlayContent}>
-          <Text style={[styles.stadiumText, { fontSize: scaleFont(36) }]}>
+          {/* <Text style={[styles.stadiumText, {fontSize: scaleFont(36)}]}>
             Wankhede Stadium
-          </Text>
+          </Text> */}
         </View>
       </LinearGradient>
 
       {/* Player Stats */}
       <LinearGradient
         colors={['#2a0a3d', '#531d8a']}
-        style={[styles.box, { flex: 0.15 * (height / 667) }]}>
+        style={[styles.box, {flex: 0.20 * (height / 667)}]}>
         <View style={styles.playerStats}>
-          <Text style={[styles.statItem, { fontSize: scaleFont(26) }]}>Kohli: 78*(45)</Text>
-          <Text style={[styles.statItem, { fontSize: scaleFont(26) }]}>Strike Rate: 173.3</Text>
-          <Text style={[styles.statItem, { fontSize: scaleFont(26) }]}>4s/6s: 8/4</Text>
+          <Text style={[styles.statItem, {fontSize: scaleFont(26)}]}>
+            {strikerText}
+          </Text>
+          <Text style={[styles.statItem, {fontSize: scaleFont(26)}]}>
+            {strikeRateText}
+          </Text>
+          <Text style={[styles.statItem, {fontSize: scaleFont(26)}]}>
+            {foursSixesText}
+          </Text>
+        </View>
+        <View style={styles.playerStats}>
+          <Text style={[styles.statItem, {fontSize: scaleFont(26)}]}>
+            {nonStrikeText}
+          </Text>
+          <Text style={[styles.statItem, {fontSize: scaleFont(26)}]}>
+            {nonstrikeRateText}
+          </Text>
+          <Text style={[styles.statItem, {fontSize: scaleFont(26)}]}>
+            {nonfoursSixesText}
+          </Text>
+        </View>
+        <View style={styles.playerStats}>
+          <Text style={[styles.statItem, {fontSize: scaleFont(26)}]}>
+            {bowler?.bowlName}
+          </Text>
+          <Text style={[styles.statItem, {fontSize: scaleFont(26)}]}>
+            {bowlerRate}
+          </Text>
+       
         </View>
       </LinearGradient>
 
       {/* Bottom Bar */}
       <LinearGradient
         colors={['#434343', '#000000']}
-        style={[styles.box, { flex: 0.10 * (height / 667) }]}>
+        style={[styles.box, {flex: 0.2 * (height / 667)}]}>
         <View style={styles.bottomBar}>
-          <Text style={[styles.bottomText, { fontSize: scaleFont(24) }]}>
-            IND won toss | Need 38 in 67 | CRR:7.83
-          </Text>
+        <Text style={[styles.bottomText, { fontSize: scaleFont(24) }]}>
+  {`${tossText} \n LW: ${matchData?.lastWicket} \n ${matchData?.status}`}
+</Text>
+
         </View>
       </LinearGradient>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
   },
   flag: {
     overflow: 'hidden',
     borderColor: 'white',
   },
   box: {
-  
     marginVertical: scaleVertical(2),
     overflow: 'hidden',
   },
@@ -208,13 +362,12 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     zIndex: 1,
     justifyContent: 'flex-end',
-    padding: scaleSize(16),
   },
   stadiumText: {
     color: 'white',
     fontFamily: 'mudstone',
     textShadowColor: 'rgba(0,0,0,0.9)',
-    textShadowOffset: { width: 1, height: 1 },
+    textShadowOffset: {width: 1, height: 1},
     textShadowRadius: 3,
   },
   playerStats: {
@@ -231,13 +384,14 @@ const styles = StyleSheet.create({
   },
   bottomBar: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
+    flexDirection: 'row',
     paddingHorizontal: scaleHorizontal(16),
   },
   bottomText: {
     color: 'white',
-    fontFamily: 'mudstone',
+    fontFamily: 'lean',
     textAlign: 'center',
   },
   statsText: {
